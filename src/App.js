121d10f2dae66614e812b2058998e7eccaaff1f1
Jeramy Sena <jeramy_sena@hotmail.com>
2022-03-08 13:08:20 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <Home></Home>
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
