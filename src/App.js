d1f22d997fd1419ec1a866a71be1246d6206e2a7
Jeramy Sena <jeramy_sena@hotmail.com>
2022-03-10 17:04:41 -0600

    saving progress

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from '@apollo/client';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
-import GameBoard from './components/GameBoard';
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import Signup from "./pages/Signup";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-    <header>
-      <div>
-      <Header></Header>
-      </div>
-    </header>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
 
-    <section>
-      <div>
-      <GameBoard></GameBoard>
-      </div>
-    </section>      
-    
-    <footer>
-      <Footer></Footer>
-    </footer>
-    </>
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
